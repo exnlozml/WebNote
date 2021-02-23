d56c371dcdd3e53e40e72431d344f8208389ac92
chw <exnlozml@outlook.com>
2021-02-23 16:51:04 +0800

    better-scroll

--- tabbar/src/views/home/Home.vue
+++ tabbar/src/views/home/Home.vue
@@ -1,13 +1,15 @@
 <template>
-  <h2>首页</h2>
+  <!-- <h2>首页</h2> -->
+  <better-scroll></better-scroll>
 </template>
 
 <script>
+import betterScroll from '../../components/tabbar/better-scroll/better-scroll.vue'
 export default {
+  components: { betterScroll },
 
 }
 </script>
 
 <style>
-
 </style>
